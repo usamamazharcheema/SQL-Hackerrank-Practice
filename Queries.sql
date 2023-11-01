6bf49fe25150db4b9edb899afe2993240af94ef3
Usama Mazhar <usamamazhar200@gmail.com>
2023-11-01 13:14:11 +0100

    Employees filteration based on salary and employement period

--- Queries.sql
+++ Queries.sql
@@ -41,3 +41,12 @@ from the Employee table in alphabetical order.
 
 SELECT name FROM Employee
 ORDER BY name;
+
+/*
+Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee 
+having a salary greater than 2000 per month who have been employees for less than 10 months. 
+Sort your result by ascending employee_id.
+*/
+
+SELECT name FROM Employee
+WHERE salary > 2000 AND months < 10;
