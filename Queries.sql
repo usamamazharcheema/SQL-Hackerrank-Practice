24663a5b3aa77696b5570fb1957d86497efa7db5
Usama Mazhar <usamamazhar200@gmail.com>
2023-11-01 11:51:11 +0100

    Student Marks greater than 75

--- Queries.sql
+++ Queries.sql
@@ -3,19 +3,33 @@
 Your result cannot contain duplicates.
 */
 
-SELECT DISTINCT CITY FROM STATION WHERE CITY NOT RLIKE '^[aeiouAEIOU].*$'
+SELECT DISTINCT CITY FROM STATION 
+WHERE CITY NOT RLIKE '^[aeiouAEIOU].*$'
 
 /*
 Query the list of CITY names from STATION that do not end with vowels. 
 Your result cannot contain duplicates.
 */
 
-SELECT DISTINCT CITY FROM STATION WHERE RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
+SELECT DISTINCT CITY FROM STATION 
+WHERE RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
 
 /*
 Question: Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
 Your result cannot contain duplicates.
 */
 
-1. SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u') OR RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
-2. SELECT DISTINCT city FROM station WHERE city RLIKE '^[^aeiouAEIOU].*|.*[^AEIOUaeiou]$';
+1. SELECT DISTINCT CITY FROM STATION 
+   WHERE LEFT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u') OR RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
+2. SELECT DISTINCT city FROM station 
+   WHERE city RLIKE '^[^aeiouAEIOU].*|.*[^AEIOUaeiou]$';
+
+/*
+Query the Name of any student in STUDENTS who scored higher than  Marks. 
+Order your output by the last three characters of each name. 
+If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+*/
+
+SELECT name FROM STUDENTS
+WHERE Marks > 75
+ORDER BY LOWER(RIGHT(name,3)), id;
