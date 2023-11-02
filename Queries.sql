4fea8ce1637706b857b62ceb79813810a2a89a09
Usama Mazhar <usamamazhar200@gmail.com>
2023-11-02 10:28:18 +0100

    Student Grades computation based on marks

--- Queries.sql
+++ Queries.sql
@@ -59,3 +59,25 @@ their respective average city populations (CITY.Population) rounded down to the 
 SELECT country.continent, FLOOR(AVG(city.population)) FROM 
 country INNER JOIN city ON city.countrycode=country.code
 GROUP BY country.continent;
+
+/*
+You are given two tables: Students and Grades. Students contains three columns ID, Name and Marks.
+Grades contains the three columns too Grade, Min_Mark, Max_Mark
+Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. 
+Ketty doesn't want the NAMES of those students who received a grade lower than 8. 
+The report must be in descending order by grade -- i.e. higher grades are entered first. 
+If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically.
+Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. If there is more than one 
+student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
+
+Write a query to help Eve.
+*/
+
+SELECT 
+CASE WHEN g.grade < 8 THEN NULL
+ELSE s.name END AS 'name'
+, g.grade
+, s.marks 
+FROM students s
+INNER JOIN grades g ON s.marks BETWEEN g.min_mark AND g.max_mark
+ORDER BY g.grade DESC, name ASC, s.marks ASC;
