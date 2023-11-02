8ed846a6ca557ac0c57660724825f77bb66a798e
Usama Mazhar <usamamazhar200@gmail.com>
2023-11-02 18:09:51 +0100

    Top Competitors

--- Queries.sql
+++ Queries.sql
@@ -81,3 +81,20 @@ ELSE s.name END AS 'name'
 FROM students s
 INNER JOIN grades g ON s.marks BETWEEN g.min_mark AND g.max_mark
 ORDER BY g.grade DESC, name ASC, s.marks ASC;
+
+/*
+ulia just finished conducting a coding contest, and she needs your help assembling the leaderboard! 
+Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than 
+one challenge. Order your output in descending order by the total number of challenges in which the hacker 
+earned a full score. If more than one hacker received full scores in same number of challenges, then sort them
+by ascending hacker_id.
+*/
+
+SELECT h.hacker_id, h.name FROM Hackers h
+JOIN Submissions s ON h.hacker_id = s.hacker_id
+JOIN Challenges c on s.challenge_id = c.challenge_id
+JOIN Difficulty d on c.difficulty_level = d.difficulty_level
+WHERE s.score = d.score
+GROUP BY h.hacker_id, h.name
+HAVING COUNT(s.challenge_id) > 1
+ORDER BY COUNT(s.challenge_id) DESC, h.hacker_id;
