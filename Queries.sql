c036eba35b90a209148f6f162312e7aac2c7e04b
Usama Mazhar <usamamazhar200@gmail.com>
2023-11-02 22:48:45 +0100

    The Blunder

--- Queries.sql
+++ Queries.sql
@@ -98,3 +98,13 @@ WHERE s.score = d.score
 GROUP BY h.hacker_id, h.name
 HAVING COUNT(s.challenge_id) > 1
 ORDER BY COUNT(s.challenge_id) DESC, h.hacker_id;
+
+/*
+Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, 
+but did not realize her keyboard's  key was broken until after completing the calculation. She wants your 
+help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual 
+average salary. Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up 
+to the next integer.
+*/
+
+SELECT CEIL((AVG(salary)) - (AVG(REPLACE(salary, '0', '')))) AS avg_salary FROM employees;
