c6d61ff8e097650bc4e0893204d598943f60f3bb
Usama Mazhar <usamamazhar200@gmail.com>
2023-11-03 21:03:32 +0100

    The Euclidean distance

--- Queries.sql
+++ Queries.sql
@@ -149,7 +149,7 @@ ORDER BY lat_n DESC
 LIMIT 1;
 
 /*
-Consider P1(a,b) and nP2(c,d) to be two points on a 2D plane.
+Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
  a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
  b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
  c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
@@ -158,3 +158,14 @@ Query the Manhattan Distance between points P1 and P2 and round it to a scale of
 */
 
 SELECT ROUND((MAX(lat_n) - MIN(lat_n)) + (MAX(long_w) - MIN(long_w)), 4) FROM station;
+
+/*
+Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
+ a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+ b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+ c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+ d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Euclidean Distance between points P1 and P2 and round it to a scale of 4 decimal places.
+*/
+
+SELECT ROUND(SQRT(POWER((MAX(lat_n) - MIN(lat_n)), 2) + POWER((MAX(long_w) - MIN(long_w)), 2)), 4) FROM station;
