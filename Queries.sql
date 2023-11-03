6de3e7658af7bdb3cb35ab32235b4b5db9603738
Usama Mazhar <usamamazhar200@gmail.com>
2023-11-03 20:36:23 +0100

    The Manhattan distance

--- Queries.sql
+++ Queries.sql
@@ -147,3 +147,14 @@ SELECT ROUND(lat_n, 4) FROM station
 WHERE lat_n < 137.2345
 ORDER BY lat_n DESC
 LIMIT 1;
+
+/*
+Consider P1(a,b) and nP2(c,d) to be two points on a 2D plane.
+ a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+ b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+ c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+ d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places.
+*/
+
+SELECT ROUND((MAX(lat_n) - MIN(lat_n)) + (MAX(long_w) - MIN(long_w)), 4) FROM station;
