e94c693a861208d3c8be83f05429c7fe104b1a17
Usama Mazhar <usamamazhar200@gmail.com>
2023-11-03 19:28:13 +0100

    Top Earners

--- Queries.sql
+++ Queries.sql
@@ -117,3 +117,15 @@ where the CONTINENT is 'Asia'.
 SELECT SUM(CITY.population) FROM city
 JOIN Country ON CITY.CountryCode = COUNTRY.Code
 WHERE COUNTRY.CONTINENT = 'Asia';
+
+/*
+We define an employee's total earnings to be their monthly salary * months  worked, and the maximum total earnings to be the maximum 
+total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees
+as well as the total number of employees who have maximum total earnings. 
+Then print these values as 2 space-separated integers.
+*/
+
+SELECT months*salary, COUNT(*) FROM employee
+GROUP BY months*salary
+ORDER BY months*salary DESC
+LIMIT 1;
