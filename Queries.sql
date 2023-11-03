993284a6eadda1e32d6130c34ca0978bf6f1f094
Usama Mazhar <usamamazhar200@gmail.com>
2023-11-03 19:59:30 +0100

    Greatest Latitude value

--- Queries.sql
+++ Queries.sql
@@ -134,5 +134,16 @@ LIMIT 1;
 Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. 
 Truncate your answer to 4 decimal places.
 */
+
 SELECT ROUND(SUM(lat_n), 4) FROM station
 WHERE lat_n BETWEEN 38.7880 AND 137.2345;
+
+/*
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. 
+Truncate your answer to 4 decimal places.
+*/
+
+SELECT ROUND(lat_n, 4) FROM station
+WHERE lat_n < 137.2345
+ORDER BY lat_n DESC
+LIMIT 1;
