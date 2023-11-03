df30f45c54966bcd80551080410cfa7e7896a7fe
Usama Mazhar <usamamazhar200@gmail.com>
2023-11-03 19:35:43 +0100

    Sum of latitude

--- Queries.sql
+++ Queries.sql
@@ -129,3 +129,10 @@ SELECT months*salary, COUNT(*) FROM employee
 GROUP BY months*salary
 ORDER BY months*salary DESC
 LIMIT 1;
+
+/*
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. 
+Truncate your answer to 4 decimal places.
+*/
+SELECT ROUND(SUM(lat_n), 4) FROM station
+WHERE lat_n BETWEEN 38.7880 AND 137.2345;
