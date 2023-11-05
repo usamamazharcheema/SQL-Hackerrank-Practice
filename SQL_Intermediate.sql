15e2dd30b8f457b3cdb482b02ac28afabe79c9f5
Usama Mazhar <usamamazhar200@gmail.com>
2023-11-05 11:29:55 +0100

    Binary Tree Nodes

--- SQL_Intermediate.sql
+++ SQL_Intermediate.sql
@@ -20,3 +20,19 @@ FROM
   cte_lat
 WHERE
   cte_lat.rowindex IN (FLOOR(@rowindex / 2) , CEIL(@rowindex / 2));
+
+/*
+You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
+Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.
+*/
+
+SELECT  CASE 
+        WHEN p IS NULL THEN CONCAT(n, ' Root')
+        WHEN n IN (SELECT DISTINCT p FROM bst) THEN CONCAT(n, ' Inner')
+        ELSE CONCAT(n, ' Leaf')
+    END
+FROM bst
+ORDER BY n ASC;
