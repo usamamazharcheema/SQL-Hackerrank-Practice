b9f5bca0c16ee49a05090b73ffab6d2b4b877d05
Usama Mazhar <usamamazhar200@gmail.com>
2023-11-08 23:06:46 +0100

    Type of Triangles

--- SQL_basic.sql
+++ SQL_basic.sql
@@ -169,3 +169,19 @@ Query the Euclidean Distance between points P1 and P2 and round it to a scale of
 */
 
 SELECT ROUND(SQRT(POWER((MAX(lat_n) - MIN(lat_n)), 2) + POWER((MAX(long_w) - MIN(long_w)), 2)), 4) FROM station;
+
+/*
+Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. 
+Output one of the following statements for each record in the table:
+Equilateral: It's a triangle with 3 sides of equal length.
+Isosceles: It's a triangle with 2 sides of equal length.
+Scalene: It's a triangle with 3 sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+*/
+
+SELECT CASE
+       WHEN (A+B) <= C OR (B+C) <= A OR (A+C) <= B THEN 'Not A Triangle' 
+       WHEN A = B AND B = C AND A = C THEN 'Equilateral'
+       WHEN A = B OR B = C OR A = C THEN 'Isosceles' 
+       ELSE 'Scalene' END AS QuantityText
+FROM TRIANGLES;
